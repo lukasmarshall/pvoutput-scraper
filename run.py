a3e123f896932530de3ba3a26adbc9d14b0c9bb4
Sokkka <raymondcarlaw@gmail.com>
2019-07-01 01:10:43 +1000

    created a class to handle sessions

--- run.py
+++ run.py
@@ -3,70 +3,118 @@ import re
 import pendulum
 import json
 from collections import namedtuple
+from time import sleep, time
 
 IntradayData = namedtuple('IntradayData', 'headers data')
 DataPoint = namedtuple('DataPoint', ['Time', 'EnergyOut', 'PowerOut', 'PowerAvg', 'EnergyIn', 'EnergyNet', 'PowerIn', 'Export', 'Import', 'Volt'])
 
 _host = 'https://pvoutput.org'
 
-def getSession(username: str, password: str):
-    s = requests.Session()
-    r = s.post(f'{_host}/index.jsp', {'login': username, 'password': password})
-    if r.status_code != 302:
-        print("Login was unsuccessful")
-        s.close()
 
-    return s
+class PVOutput:
 
-def closeSession(session):
-    s.get(f'{_host}/logout.jsp')
-    s.close()
+    def __init__(self):
+        self.request_delay = 2 # seconds delay between requests
+        self.last_request_time = time()
 
-def getIntradayData(requestedDate: pendulum.DateTime, siteId: str, session=None):
 
-    # Weirdly, to get a day's data, you request the next day in the API... Yep
-    dateString = requestedDate.add(days=1).format('YYYYMMDD')
-    url = f'{_host}/intraday.jsp?id=&sid={siteId}&dt={dateString}&gs=0&m=0'
-    if not session:
-        r = requests.get(url)
-    else:
-        r = session.get(url)
+    def login(self, username: str, password: str):
+        s = requests.Session()
+        s.headers.update({
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+            'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
+            'Connection': 'keep-alive',
+            'Host': 'pvoutput.org',
+            'Referer': 'https://pvoutput.org/ladder.jsp' 
+        })
+        r = s.post(f'{_host}/index.jsp', {'login': username, 'password': password})
+        if 'logout' not in str(r.content):
+            # may be due to captcha
+            print("Login was unsuccessful", r.status_code, r.content)
+            s.close()
 
-    # list of variable names in js script we want
-    varNames = ('dataEnergyOut', 'dataPowerOut', 'dataPowerAvg', 'dataEnergyIn',
-        'dataEnergyNet', 'dataPowerIn', 'dataExport', 'dataImport', 'dataVolt')
-    dataPoints = 0
-    data = {}
+        self.session = s
 
-    # Grab the times js string. 
-    for match in re.finditer(r'var (\w+) = \[(.*?)\];', str(r.content), re.M|re.I):
-        varName, dataString = match.group(1, 2)
-        if (varName in varNames or varName == 'timeArray') and dataString:
-            dataList = dataString.split(',')
-            if len(dataList) > 0:
-                data[varName] = dataList
-            
-            if len(dataList) > dataPoints:
-                dataPoints = len(dataList)
+        self._delay()
+        s.get(f'{_host}/ladder.jsp')
 
-    if 'timeArray' not in data:
-        raise NameError('timeArray is missing from page, this program may be out of date.')
-
-    for varName in varNames:
-        if varName not in data:
-            data[varName] = [0 for x in range(0, dataPoints)]
+    def _closeSession(self):
+        self.session.get(f'{_host}/logout.jsp')
+        self.session.close()
+        del self.session
     
-    dataList = []
+    def _delay(self):
+        time_diff = time() - self.last_request_time 
+        self.last_request_time = time()
+        if time_diff < self.request_delay:
+            sleep(self.request_delay - time_diff)
 
-    # Convert each time to a pendulum object
-    for idx, val in enumerate(data['timeArray']):
-        hour = int(val) // 60; minute = int(val) % 60
-        dataPoint = [requestedDate.set(hour=hour,minute=minute)]
+    def _getIntradayPage(self, requestedDate: pendulum.DateTime, siteId: str):
+        # Weirdly, to get a day's data, you request the next day in the API... Yep
+        dateString = requestedDate.add(days=1).format('YYYYMMDD')
+        url = f'{_host}/intraday.jsp?id=&sid={siteId}&dt={dateString}&gs=0&m=0'
+        # self._delay()
+        if not self.session:
+            r = requests.get(url)
+        else:
+            r = self.session.get(url)
+        
+        return r
+
+    def getIntradayData(self, requestedDate: pendulum.DateTime, siteId: str):
+
+        r = self._getIntradayPage(requestedDate, siteId)
+
+        if 'timeArray' not in str(r.content):
+            print("Page content is incorrect, please check the page.")
+            action = input('Try again? (Y/N)')
+            if action.lower() == 'y':
+                r = self._getIntradayPage(requestedDate, siteId)
+            else:
+                exit()
+
+        # list of variable names in js script we want
+        varNames = ('dataEnergyOut', 'dataPowerOut', 'dataPowerAvg', 'dataEnergyIn',
+            'dataEnergyNet', 'dataPowerIn', 'dataExport', 'dataImport', 'dataVolt')
+        dataPoints = 0
+        data = {}
+
+
+        # Grab the times js string. 
+        for match in re.finditer(r'var (\w+) = \[(.*?)\];', str(r.content), re.M|re.I):
+            varName, dataString = match.group(1, 2)
+            if (varName in varNames or varName == 'timeArray') and dataString:
+                dataList = dataString.split(',')
+                if len(dataList) > 0:
+                    data[varName] = dataList
+                
+                if len(dataList) > dataPoints:
+                    dataPoints = len(dataList)
+
+        if 'timeArray' not in data:
+            raise NameError('timeArray is missing from page, this program may be out of date.')
+
         for varName in varNames:
-            dataPoint.append(float(data[varName][idx]) if data[varName][idx] != 'null' else None)
+            if varName not in data:
+                data[varName] = [0 for x in range(0, dataPoints)]
+        
+        dataList = []
 
-        dataList.append(DataPoint(*dataPoint))
+        # Convert each time to a pendulum object
+        for idx, val in enumerate(data['timeArray']):
+            hour = int(val) // 60; minute = int(val) % 60
+            dataPoint = [requestedDate.set(hour=hour,minute=minute)]
+            for varName in varNames:
+                dataPoint.append(float(data[varName][idx]) if data[varName][idx] != 'null' else None)
 
-    headers = ('Time', 'Energy Out', 'Power Out', 'Power Avg', 'Energy In', 'Energy Net', 'Power In', 'Export', 'Import', 'Volt')
+            dataList.append(DataPoint(*dataPoint))
 
-    return IntradayData(headers, dataList)
+        headers = ('Time', 'Energy Out', 'Power Out', 'Power Avg', 'Energy In', 'Energy Net', 'Power In', 'Export', 'Import', 'Volt')
+
+        return IntradayData(headers, dataList)
+
+    def __del__(self):
+        if hasattr(self, 'session'):
+            self._closeSession()
+
--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-from run import getIntradayData, getSession, closeSession
+from run import PVOutput
 from pendulum import DateTime
 import csv
 from pathlib import Path
 
-siteId = '40873'
+siteId = '57775'
 
 directory = Path('directory'+siteId)
 if not directory.exists():
@@ -12,17 +12,23 @@ if not directory.exists():
 # if not logged in then this will only work for th last 14 days
 testDate = DateTime(2019, 2, 1)
 
-sesh = getSession('username', 'password')
+pvo = PVOutput()
+pvo.login('username', 'password')
 
-for idx in range(1,130):
+for idx in range(1,140):
     dateString = testDate.to_date_string()
     print('creating file ', dateString)
-    data = getIntradayData(testDate, siteId, sesh)
+    try:
+        data = pvo.getIntradayData(testDate, siteId)
+    except NameError as e:
+        print(e)
+        print("missing data for " + dateString)
+        testDate = testDate.add(days=1)
+        continue
+
     with open(directory.as_posix() + f'/{dateString}.csv','w', newline='') as csvFile:
         dataFile = csv.writer(csvFile)
         dataFile.writerow(data.headers)
         dataFile.writerows(data.data)
 
     testDate = testDate.add(days=1)
-
-sesh.close()
